ed157f7ff835b9bd8834e16497579da47bba87b5
Umar-ml <umarfarooq9886844@gmail.com>
2025-02-14 17:59:31 +0500

    changes in cu

--- control_unit/cu.v
+++ control_unit/cu.v
@@ -1,8 +1,16 @@
 module sop (
     input [31:0] data_in,
-    output addr, sub, sllr, sltr, sltur, xorr, srlr, srar, orr,andr
+    output addr, sub, sllr, sltr, sltur, xorr, srlr, srar, orr,andr,
     output addi,slli, slti, sltui, xori, srli, srai, ori, andi,
-    output sw,sh,sb,lb,lh,lw,lbu,lhu
+    output sw,sh,sb,lb,lh,lw,lbu,lhu,
+    output jal, jalr,
+    output beq, bne, blt, bge, bltu, bgeu,
+    output add, sll, slt, sltu, xorrr, srl, sra, orrr, andd,
+    output out0, out1,out2,out3,
+    output wenb, rs2_imm_sel,
+    output reg [3:0] sel_bit,
+    output lui_enb, auipc_wenb, load_enb, jal_enb, branch_enb, in_to_pr
+    output reg [1:0] sel_bit_mux
 );
     wire [8:0] selected_bits;
     assign i0 = data_in[2];
@@ -44,6 +52,57 @@ module sop (
     assign lw = (~i0)&(~i1)&(~i2)&(~i3)&(~i4)&(~i5)&(i6)&(~i7)&(~i8);
     assign lbu = (~i0)&(~i1)&(~i2)&(~i3)&(~i4)&(~i5)&(i6)&(i7)&(~i8);
     assign lhu = (~i0)&(~i1)&(~i2)&(~i3)&(~i4)&(i5)&(~i6)&(i7)&(~i8);
+    // load enable 
+    assign load_enb = (lb) | (lh) | (lw) | (lbu) | (lhu);
+    //Jump instructions
+    assign jal = (i0)&(i1)&(~i2)&(i3)&(i4)&(~i5)&(~i6)&(~i7)&(~i8);
+    assign jalr = (i0)&(~i1)&(~i2)&(i3)&(i4)&(~i5)&(~i6)&(~i7)&(~i8);
+    // enable for jal
+    assign jal_enb = (jal) | (jalr);
+    //auipc enable lui enable
+    assign lui_enb = (i0)&(~i1)&(i2)&(i3)&(~i4);
+    assign auipc_wenb = (i0)&(~i1)&(i2)&(~i3)&(~i4);
+    //Branch instructions
+    assign beg = (~i0)&(~i1)&(~i2)&(i3)&(i4)&(~i5)&(~i6)&(~i7)&(~i8);
+    assign bne = (~i0)&(~i1)&(~i2)&(i3)&(i4)&(i5)&(~i6)&(~i7)&(~i8);
+    assign blt = (~i0)&(~i1)&(~i2)&(i3)&(i4)&(~i5)&(~i6)&(i7)&(~i8);
+    assign bge = (~i0)&(~i1)&(~i2)&(i3)&(i4)&(i5)&(~i6)&(i7)&(~i8);
+    assign bltu = (~i0)&(~i1)&(~i2)&(i3)&(i4)&(~i5)&(i6)&(i7)&(~i8);
+    assign bgeu = (~i0)&(~i1)&(~i2)&(i3)&(i4)&(i5)&(i6)&(i7)&(~i8);
+    // Enable for branch
+    assign branch_enb = (beg) | (bne) | (blt) | (bge) | (bltu) | (bgeu);
+    //Selection bit for alu
+    assign add = (addr) | (addi);
+    assign sll = (sllr) | (slli);
+    assign slt = (sltr) | (slti);
+    assign sltu = (sltur) | (sltui);
+    assign xorrr = (xorr) | (xori);
+    assign srl = (srlr) | (srli);
+    assign sra = (srar) | (srai);
+    assign orrr = (orr) | (ori);
+    assign andd = (andr) | (andi)
+    assign out0 = (sll) | (sltu) | (srl) | (sra) | (andd);
+    assign out1 = (slt) | (sltu) | (orrr) | (andd);
+    assign out2 = (xorrr) | (srl) | (sra) | (orrr) | (andd);
+    assign out3 = (sub) | (sra);
+    always @(*)
+    begin
+    	sel_bit = {out0, out1, out2, out3};
+    end
 
+    // write enable and rs2 immediate selection
+    assign wenb = (lw) | (jal) | (lh) | (lb) | (addr) | (sub) | (srar) | (sllr) | (orr) | (andr) | (sltur) | (sltr) | (srai) | (xorr) | (srlr) | (andi) | (auipc_wenb) | (ori) | (xori) | (sltui) | (srli) | (slli) | (addi) | (slti) | (sb) | (sh) | (sw) | (lbu) | (lhu) | (jalr) | (lui);
+    assign rs2_imm_sel = (lui_enb) | (jal) | (lb) | (lh) |(addi) | (sh) | (sb) | (sw) | (slli) | (srai) | (auipc_wenb) | (ori) | (andi) | (srli) | (xori) | (sltui) | (slti) | (lbu) | (lhu) | (jalr) | (lw);
+    // Select bit for mux
+    assign in_to_pr = ~(jal | jalr | branch_enb)
+    always @(*) 
+    begin
+    	casez({jal, jalr, branch_enb, in_to_pr)
+    		4'b1??? : sel_bit_mux = 2'b11;
+    		4'b01?? : sel_bit_mux = 2'b10;
+    		4'b001? : sel_bit_mux = 2'b01;
+    		4'b0001 : sel_bit_mux = 2'b00;
+    	endcase
+    end
 
 endmodule
