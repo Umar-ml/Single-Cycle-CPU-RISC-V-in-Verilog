73e2fcc619cfb0aa50b3b5c34f6f0e7e8e629ba6
Umar-ml <umarfarooq9886844@gmail.com>
2025-02-15 19:43:42 +0500

    add the branch instructions to ALU

--- ALU/alu.v
+++ ALU/alu.v
@@ -16,6 +16,14 @@ module alu(
 			4'b0111: out = dataA & dataB; // and out
 			4'b1000: out = dataA - dataB; //sub
 			4'b1001: out = $signed(dataA) >>> dataB[4:0]; //SRA
+			// Branch operations
+			4'b1010: out = (dataA == dataB) ? 32'b1 : 32'b0; // BEQ
+            		4'b1011: out = (dataA != dataB) ? 32'b1 : 32'b0; // BNE
+            		4'b1100: out = ($signed(dataA) < $signed(dataB)) ? 32'b1 : 32'b0; // BLT
+            		4'b1101: out = ($signed(dataA) >= $signed(dataB)) ? 32'b1 : 32'b0; // BGE
+            		4'b1110: out = (dataA < dataB) ? 32'b1 : 32'b0; // BLTU (unsigned)
+            		4'b1111: out = (dataA >= dataB) ? 32'b1 : 32'b0; // BGEU (unsigned)
+			
 			default: out = 32'b0;
 		endcase
 	end
